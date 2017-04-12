cf63cdf5a1648c9876a9cb4c91078f3c7c637dcc
ahlumin <magus52941@gmail.com>
2017-04-12 21:56:39 +0800

    reset

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="use-absolute">
-    <div>
-        <img src="//avatars2.githubusercontent.com/u/11625554?v=3&u=07a232ea8026737acf50ad2515dd09c3da7702ae&s=400" />
-    </div>
+        <img src="https://avatars1.githubusercontent.com/u/11625554?v=3&s=460" />
   </div>
 </template>
 
@@ -13,14 +11,14 @@
     width:100%;
     height:100%;
 }
-.use-absolute div{
+.use-absolute img{
     position: absolute;
     top:0;
     right:0;
     bottom:0;
     left:0;
+    margin-left:-102px;
+    margin-top:-102px;
     margin:auto;
-    width:100px;
-    height:50px;
 }
 </style>
